6fa9fe11a17cf6a08a1cfb5e356cfa9582ce48b9
Matthew Bull <mattbull33@gmail.com>
2023-06-21 09:40:51 -0400

    added tag model to get requests inproduct, fix typo in get by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const getProduct = await Product.findAll({
-      include: [{ model: Category}]
+      include: [{ model: Category}, { model: Tag}]
     });
     res.status(200).json(getProduct)
   } catch (err) {
@@ -19,8 +19,8 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   try {
-    const oneProduct = await Product.findByPk({
-      include: [{ model: Category}]
+    const oneProduct = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag} ]
     });
     res.status(200).json(oneProduct)
   } catch (err) {
